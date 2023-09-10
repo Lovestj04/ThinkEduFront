7eeb6dcb60af05d9ef9b6a2a234a6983da242ac7
Lovestj04 <jhonnigomez93@gmail.com>
2023-09-10 18:27:07 -0300

    se creo la rama login y se agrego la base de un login

--- ThinkEduFront/src/components/layout/navbar/navbar.jsx
+++ ThinkEduFront/src/components/layout/navbar/navbar.jsx
@@ -6,11 +6,11 @@ function ColorSchemesExample() {
       <>
         <Navbar style={{ backgroundColor: '#9977AA'}} data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">ThinkEdu</Navbar.Brand>
+            <Navbar.Brand href="/">ThinkEdu</Navbar.Brand>
             <Nav className="ml-0">
-              <Nav.Link href="#home" className="mx-2">Home</Nav.Link>
-              <Nav.Link href="#features" className="mx-2">Students</Nav.Link>
-              <Nav.Link href="#pricing" className="mx-2">Subjects</Nav.Link>
+              <Nav.Link href="/" className="mx-2">Home</Nav.Link>
+              <Nav.Link href="/Studentstable" className="mx-2">Students</Nav.Link>
+              <Nav.Link href="/Subjects" className="mx-2">Subjects</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
