0d683db292ea93546308f9784020726b91a0e199
franco <soyfrancocaliao@gmail.com>
2023-09-11 12:58:34 -0300

    se agrego footer y se lo enruto

--- ThinkEduFront/src/App.jsx
+++ ThinkEduFront/src/App.jsx
@@ -3,9 +3,11 @@ import Aboutus from "../src/components/pages/Aboutus"
 import Subjects from "../src/components/pages/Subjects"
 import Error from "../src/components/pages/Error404"
 import Home from "../src/components/pages/home/Home"
-import Login from "../src/components/pages/login/Login"
+import Login from "./components/pages/login/Login.jsx"
 import Studentstable from "../src/components/pages/stundentstable/studentstable"
 import Navbar from "../src/components/layout/navbar/navbar"
+import Footer from "./components/layout/Footer/footer"
+import './app.css'
 
 const App = () => {
   return (
@@ -16,7 +18,7 @@ const App = () => {
         <Routes>
           <Route>
           <Route exact path="/" element={<Home />} />
-      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/Login" element={<Login />} />
       <Route exact path="/Studentstable" element={<Studentstable />} />
       <Route exact path="/Subjects" element={<Subjects />} />
       <Route exact path="/Aboutus" element={<Aboutus />} />
@@ -37,7 +39,7 @@ const App = () => {
           
         
       </BrowserRouter>
-    
+    <Footer/>
   
     </div>
   )
--- ThinkEduFront/src/components/pages/login/Login.jsx
+++ ThinkEduFront/src/components/pages/login/Login.jsx
@@ -1,34 +1,32 @@
 import React from 'react'
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 const Login = () => {
-  return (
     <Container>
-      <Row className="justify-content-center mt-5">
-        <Col xs={6}>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Usuario</Form.Label>
-              <Form.Control type="text" placeholder="Ingresa tu usuario" />
-            </Form.Group>
+    <Row className="justify-content-center mt-5">
+      <Col xs={6}>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Usuario</Form.Label>
+            <Form.Control type="text" placeholder="Ingresa tu usuario" />
+          </Form.Group>
 
-            <Form.Group controlId="formBasicPassword" className='mb-3'>
-              <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
-            </Form.Group>
+          <Form.Group controlId="formBasicPassword" className='mb-3'>
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="password" placeholder="Contraseña" />
+          </Form.Group>
 
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Recordar contraseña" />
-            </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Recordar contraseña" />
+          </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Iniciar Sesión
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
+          <Button variant="primary" type="submit">
+            Iniciar Sesión
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+  </Container>
+
 };
 
-export default Login;
+export default Login
