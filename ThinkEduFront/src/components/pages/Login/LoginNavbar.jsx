5a4678aa0427e8cac43659b799951a9557ec5c1b
Lovestj04 <jhonnigomez93@gmail.com>
2023-09-16 17:11:36 -0300

    se agregan iconos al navbar en todas las rutas

--- ThinkEduFront/src/components/pages/Login/LoginNavbar.jsx
+++ ThinkEduFront/src/components/pages/Login/LoginNavbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container, Navbar } from "react-bootstrap";
+import {PiBrain} from "react-icons/pi"
 
 function LoginNavbar() {
   return (
     <>
       <Navbar style={{ backgroundColor: "#9977AA" }} data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">ThinkEdu</Navbar.Brand>
+          <Navbar.Brand href="/"><PiBrain size="2rem" className="mb-2"/> ThinkEdu</Navbar.Brand>
         </Container>
       </Navbar>
     </>
--- ThinkEduFront/src/components/pages/home/Navbarhome.jsx
+++ ThinkEduFront/src/components/pages/home/Navbarhome.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { AiFillHome } from "react-icons/ai"; 
+import { PiBrain, PiStudentBold } from "react-icons/pi";
+import { BiBookBookmark, BiHomeAlt2 } from "react-icons/bi";
+import { MdLogout } from "react-icons/md";
 
 function ColorSchemesExample() {
   return (
     <>
-      <Navbar style={{ backgroundColor: '#9977AA'}} data-bs-theme="dark">
+      <Navbar style={{ backgroundColor: '#9977AA' }} data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">ThinkEdu</Navbar.Brand>
-          <Nav className="ml-0">
+          <Navbar.Brand href="/"> <PiBrain size="2rem" className="mb-2"/> ThinkEdu</Navbar.Brand>
+          <Nav className="mx-auto">
             <Nav.Link href="/Home" className="mx-2">
-            <AiFillHome /> Home
+              <BiHomeAlt2 size="2rem" className="mb-2"/> Home
             </Nav.Link>
-            <Nav.Link href="/Studentstable" className="mx-2">Students</Nav.Link>
-            <Nav.Link href="/Subjects" className="mx-2">Subjects</Nav.Link>
+            <Nav.Link href="/Studentstable" className="mx-2"> <PiStudentBold size="2rem" className="mb-2" />Students</Nav.Link>
+            <Nav.Link href="/Subjects" className="mx-2"> <BiBookBookmark size="2rem" className="mb-2"/> Subjects </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link className="ml-2"><MdLogout size="2rem" className="mb-2" />Logout</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
     </>
   );
 }
+
 
 export default ColorSchemesExample;
